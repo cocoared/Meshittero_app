1e08b5fd8f1c295ceadee7a06e5405c5eb361523
cocoared <dmm23program@gmail.com>
2022-02-18 09:48:33 +0000

    [Finish]4-24

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,8 +3,8 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
-  validates :shop_name,presence: true
+
+  validates :shop_name, presence: true
   validates :image, presence: true
 
   def get_image
@@ -14,7 +14,7 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
